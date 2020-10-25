1cbdead5caaaf1b258aa0612fc2dd66cf6ca3ddd
Loic Vourch <code@verybadfrags.com>
2020-10-25 13:16:00 -0700

    Clean Python bundle script

--- bundle.py
+++ bundle.py
@@ -1,20 +1,19 @@
 source_folder = "src/"
 build_folder = "build/"
 
-wrapper_path = source_folder + "wrapper.html"
+input_wrapper_path = source_folder + "wrapper.html"
+input_content_path = build_folder + "content.html"
 content_tag = "<!-- Content -->"
-
-content_path = build_folder + "content.html"
 
 output_path = build_folder + "index.html"
 
 # Get Wrapper
-wrapper_file = open(wrapper_path, "r")
+wrapper_file = open(input_wrapper_path, "r")
 wrapper = wrapper_file.read()
 wrapper_file.close()
 
 # Get content
-content_file = open(content_path, "r")
+content_file = open(input_content_path, "r")
 content = content_file.read()
 content_file.close()
 
@@ -26,4 +25,4 @@ output_file = open(output_path, "w")
 output_file.write(output_html)
 output_file.close()
 
-print("Bundled " + content_path + " into " + output_path)
+print("Bundled " + input_content_path + " into " + output_path)
