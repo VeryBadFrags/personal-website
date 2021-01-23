021b6896a9ff7285835792a96a65911fb51d5dc5
Loic Vourch <code@verybadfrags.com>
2021-01-22 22:33:55 -0800

    Update favicon

--- snowpack.config.js
+++ snowpack.config.js
@@ -17,6 +17,8 @@ module.exports = {
   <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes" />
   <title>${frontmatter.title || ''}</title>
   <meta property="og:site_name" content="Loic Vourch" />
+  <link rel="icon" type="image/svg+xml" href="favicon.svg">
+  <link rel="alternate icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👨‍💻</text></svg>" />
   <link rel="stylesheet" href="${frontmatter.output?.html_document?.css}">
 </head>
 <body>
