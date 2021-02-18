e1ab68b129fee9c4fc9bd9c26b4a4cad80ce0c94
VeryBadFrags <1664052+VeryBadFrags@users.noreply.github.com>
2021-02-17 19:14:18 -0800

    Remove Optional chaining

--- snowpack.config.js
+++ snowpack.config.js
@@ -11,7 +11,7 @@ function getHTMLWrapper(frontmatter, html) {
   <meta property="og:site_name" content="Loic Vourch" />
   <link rel="icon" type="image/svg+xml" href="favicon.svg">
   <link rel="alternate icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👨‍💻</text></svg>" />
-  <link rel="stylesheet" href="${frontmatter.output?.html_document?.css}">
+  <link rel="stylesheet" href="${frontmatter.output.html_document.css}">
 </head>
 <body>
   <div class="container">
