117453cd44b44f5e2aecc259d96a54a739fe4379
VeryBadFrags <1664052+VeryBadFrags@users.noreply.github.com>
2021-11-14 08:18:33 -0800

    Add no-console to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 module.exports = {
     "env": {
         "browser": true,
@@ -18,6 +19,7 @@ module.exports = {
         "react"
     ],
     "rules": {
-        "react/prop-types" : [0]
+        "no-console": 1,
+        "react/prop-types" : [0],
     }
 };
