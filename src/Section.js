eee2ec04b9a64c08ad01122632bdcdd3e13538bf
VeryBadFrags <1664052+VeryBadFrags@users.noreply.github.com>
2021-12-27 22:44:56 -0800

    Update Section colors

--- src/Section.js
+++ src/Section.js
@@ -3,10 +3,10 @@ import React from "react";
 
 export default function Section({ icon, title, children }) {
   return (
-    <Box my="lg">
+    <Box id={`section-${title}`} my="lg">
       {title ? (
-        <Heading as="h2" size="xl" color="pink">
-          {icon} <span className="drac-text-pink-purple">{title}</span>
+        <Heading as="h2" size="xl" color="purple">
+          {icon} <span className="drac-text-purple-cyan">{title}</span>
         </Heading>
       ) : null}
       {children}
