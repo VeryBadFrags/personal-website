eb7003b5c6317d5183ff8fc51c7c6c9929ac5a5c
VeryBadFrags <1664052+VeryBadFrags@users.noreply.github.com>
2023-01-08 11:46:44 -0800

    Move to 2-columns setup

---
+++ src/CardItem/CardItem.js
@@ -0,0 +1,74 @@
+import "./CardItem.css";
+import React from "react";
+import parse from "html-react-parser";
+import { Box, Button, Card, Heading, List, Paragraph } from "dracula-ui";
+
+export default function CardItem({ content }) {
+  return (
+    <Card
+      variant="subtle"
+      color="cyan"
+      borderColor="purple"
+      p="sm"
+      mb="md"
+      className="card-item"
+    >
+      <CardHeader content={content} />
+      <CardBody content={content} />
+      <CardFooter content={content} />
+    </Card>
+  );
+}
+
+function CardHeader({ content }) {
+  return (
+    <Box display="flex" className="card-title">
+      <Heading as="h3" color="cyan">
+        {parse(content.icon)}{" "}
+        <span className="drac-text-cyan-green">{content.title}</span>
+      </Heading>
+      {content.badge ? (
+        <Box>
+          <img
+            alt="MIT License"
+            src="https://img.shields.io/badge/license-MIT-9580ff"
+          />
+        </Box>
+      ) : null}
+    </Box>
+  );
+}
+
+function CardBody({ content }) {
+  return (
+    <Box m="xs">
+      {content.body.map((element, index) => (
+        <Paragraph pb="sm" key={index}>
+          {parse(element)}
+        </Paragraph>
+      ))}
+    </Box>
+  );
+}
+
+function CardFooter({ content }) {
+  return (
+    <List>
+      {content.links.map((link, index) => (
+        <li key={index}>
+          <Button
+            as="a"
+            href={link.url}
+            color="cyanGreen"
+            mb="xs"
+            size="sm"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {parse(link.text)}
+          </Button>
+        </li>
+      ))}
+    </List>
+  );
+}
---
+++ src/Games/Games.js
@@ -0,0 +1,25 @@
+import "./Games.scss";
+import React from "react";
+import Blocks from "../content/blocks";
+import CardItem from "../CardItem/CardItem";
+import Sand from "../content/sand";
+import OfflineSpyfall from "../content/off-spyfall";
+import OfflineWolf from "../content/off-wolf";
+import Renard from "../content/renard";
+import Spyfall from "../content/spyfall";
+import { Box } from "dracula-ui";
+
+function Games() {
+  return (
+    <Box mt="xs" className="games-container">
+      <CardItem content={Sand} />
+      <CardItem content={Spyfall} />
+      <CardItem content={Renard} />
+      <CardItem content={Blocks} />
+      <CardItem content={OfflineSpyfall} />
+      <CardItem content={OfflineWolf} />
+    </Box>
+  );
+}
+
+export default Games;
