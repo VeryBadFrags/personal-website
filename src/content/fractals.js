26f8c068c6b52ed804cbb071d87b6450ab31a2d7
VeryBadFrags <1664052+VeryBadFrags@users.noreply.github.com>
2023-01-21 09:40:02 -0800

    Add Fractals

---
+++ src/content/fractals.js
@@ -0,0 +1,14 @@
+const content = {
+  icon: '<i class="fas fa-snowflake"></i>',
+  title: "Fractals",
+  body: ["Fractals generator.", "(TypeScript)"],
+  links: [
+    {
+      url: "https://fractals.verybadfrags.com",
+      text: '<i class="fas fa-play-circle"></i> Try the project',
+    },
+  ],
+  badge: false,
+};
+
+export default content;
